b51dc079277195efda7b9d92fab7ab54f37eb611
Elaidzha1940 <eli080293@gmail.com>
2023-07-17 17:07:58 +0300

    MatchedGeometryEffect + Test

--- MatchedGeometryEffect/ContentView.swift
+++ MatchedGeometryEffect/ContentView.swift
@@ -10,16 +10,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isClicked: Bool = false
+    @Namespace private var namespace
+    
     var body: some View {
         
         VStack {
             
             if !isClicked {
-                
-                RoundedRectangle(cornerRadius: 12)
-                    .frame(width: 40, height: 40)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 150, height: 100)
+                    .matchedGeometryEffect(id: "rectangle", in: namespace)
             }
+            Spacer()
             
+            if isClicked {
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width: 80, height: 50)
+                    .matchedGeometryEffect(id: "rectangle", in: namespace)
+
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.red.opacity(0.5))
+        .onTapGesture {
+            withAnimation(.easeInOut) {
+                isClicked.toggle()
+            }
         }
     }
 }
